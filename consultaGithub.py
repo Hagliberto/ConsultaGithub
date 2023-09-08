43416fd52b066f1d4a7a347609f456b485ab67c4
Hagliberto <Hagliberto@users.noreply.github.com>
2023-09-08 05:08:09 -0300

    Update da interface
    Melhorias na interface.

--- consultaGithub.py
+++ consultaGithub.py
@@ -25,13 +25,30 @@ def exibir_perfil(infoUsuario):
 def ui():
     st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">', unsafe_allow_html=True)
     
-    st.title('Consulta Github')
-    username = st.text_input('Insira o username de usuário do Github')
+    st.title('Consultar perfis no Github')
+
+    # Espaçamento para separar o título da seção de entrada do usuário
+    st.write("")
+
+    # Organização visual usando uma coluna
+    col1, col2 = st.columns([1, 2])
+
+    with col1:
+        username = st.text_input('Insira o username de usuário do Github')
+
+    with col2:
+        st.image("https://platzi.com.br/blog/wp-content/uploads/2022/04/cover-github-105d8310-41f9-4d9e-91e0-4cbde3d8c645-50decd84-0c17-476c-b8dc-61091d0dd27f-3.webp", width=100)
     
-    if st.button('Buscar'):
+    if st.button('Buscar Perfil'):
         response = selecionarUsuario(username)
         if response.status_code == 200:
             infoUsuario = response.json()
+            
+            # Espaçamento após o botão para separar o perfil exibido
+            st.write("")
+
+            # Estilo para destacar as informações do perfil
+            st.markdown('<h2>Perfil do Usuário</h2>', unsafe_allow_html=True)
             exibir_perfil(infoUsuario)
         elif response.status_code == 404:
             st.error('Usuário não encontrado. Verifique o nome de usuário.')
